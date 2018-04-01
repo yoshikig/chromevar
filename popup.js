f0807a5281445a76df9ba5081141f43774feee1c
yoshiki iguchi <yoshiki@su.la>
2018-04-02 03:25:41 +0900

    Update the text in the window

--- popup.js
+++ popup.js
@@ -1,1 +1,4 @@
-document.querySelector('#ua').innerText = navigator.userAgent;
+var ua = window.navigator.userAgent.split(' ');
+ua = ua.filter(function(s) { return s.lastIndexOf("Chrome/", 0) === 0; });
+
+document.querySelector('#ua').innerText = ua;
